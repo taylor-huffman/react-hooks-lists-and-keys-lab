a0b60eaed793d64cd96087a71f17db2253c1d6e9
Taylor Huffman <taylor@crucialartists.com>
2022-02-25 10:42:05 -0600

    Completed assignment

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,15 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  const technologyItems = technologies.map((techItem) => {
+    return <span key={techItem}>{techItem}</span>
+  })
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {technologyItems}
       </div>
     </div>
   );
